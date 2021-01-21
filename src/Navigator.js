4ef994ddda42b677ac22eacbce5413e2bd969708
loressl <loryssl@hotmail.com>
2021-01-21 00:27:07 -0300

    Adicionado tela de login e cadastro.

---
+++ src/Navigator.js
@@ -0,0 +1,21 @@
+import {createAppContainer, createSwitchNavigator} from 'react-navigation'
+
+import TaskList from './screens/TaskList'
+import Auth from './screens/Auth'
+
+const mainRoutes ={
+    Auth:{
+        name:'Auth',
+        screen: Auth
+    },
+    Home:{
+        name: 'Home',
+        screen: TaskList
+    }
+}
+
+const mainNavigator = createSwitchNavigator(mainRoutes, {
+    initialRouteName: 'Auth'
+})
+
+export default createAppContainer(mainNavigator)
---
+++ src/screens/Auth.js
@@ -0,0 +1,142 @@
+import React, { Component } from 'react'
+import {
+    ImageBackground,
+    Text,
+    StyleSheet,
+    View,
+    TouchableOpacity,
+    Alert
+} from 'react-native'
+
+import backgroundImage from '../../assets/img/login7.jpg'
+import commonStyles from '../commonStyles'
+import AuthInput from '../components/AuthInput'
+
+export default class Auth extends Component {
+
+    state = {
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+        stageNew: false,
+    }
+
+    signinOrSignup = () => {
+        if (this.state.stageNew) {
+            Alert.alert('Sucesso!', 'Conta criada')
+        } else {
+            Alert.alert('Sucesso!', 'Logado')
+        }
+    }
+
+    render() {
+        return (
+            <ImageBackground source={backgroundImage}
+                style={styles.background}>
+                {/* <Text style={styles.title}>Tasks</Text> */}
+                <View style={styles.formContainer}>
+                    <Text style={styles.subtitle}>
+                        {this.state.stageNew ? 'Cria a sua conta' : 'Informe seus dados'}
+                    </Text>
+                    {this.state.stageNew &&
+                        <AuthInput
+                            icon='user'
+                            placeholder='Nome'
+                            value={this.state.name}
+                            style={styles.input}
+                            onChangeText={name => this.setState({ name })} />
+                    }
+                    <AuthInput
+                        icon="at"
+                        placeholder='E-mail'
+                        value={this.state.email}
+                        style={styles.input}
+                        onChangeText={email => this.setState({ email })} />
+                    <AuthInput
+                        icon="lock"
+                        placeholder='Senha'
+                        value={this.state.password}
+                        style={styles.input}
+                        secureTextEntry={true}
+                        onChangeText={password => this.setState({ password })} />
+                    {this.state.stageNew &&
+                        <AuthInput
+                            icon="asterisk"
+                            placeholder='Confirmação de Senha'
+                            value={this.state.confirmPassword}
+                            style={styles.input}
+                            secureTextEntry={true}
+                            onChangeText={confirmPassword => this.setState({ confirmPassword })} />
+                    }
+                    <TouchableOpacity activeOpacity={0.7} onPress={this.signinOrSignup}>
+                        <View style={styles.button}>
+                            <Text style={styles.buttonText}>
+                                {this.state.stageNew ? 'Registrar' : 'Entrar'}
+                            </Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+                <View style={{width: '90%', alignItems:'flex-end'}}>
+                    <TouchableOpacity style={{ padding: 10 }}
+                        onPress={() => this.setState({ stageNew: !this.state.stageNew })}
+                    >
+                        <Text style={styles.buttonTextOptions}>
+                            {this.state.stageNew ? 'Já possui conta?' : 'Ainda não possui conta?'}
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </ImageBackground>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title: {
+        fontFamily: commonStyles.fontFamily,
+        color: "black",
+        fontSize: 60,
+        marginBottom: 10,
+    },
+    subtitle: {
+        fontFamily: commonStyles.fontFamily,
+        color: '#FFF',
+        fontSize: 20,
+        textAlign: 'center',
+        marginBottom: 5
+    },
+    formContainer: {
+        backgroundColor: 'rgba( 0,0, 0, 0.8)',
+        padding: 20,
+        width: '90%',
+        borderRadius: 15
+    },
+    input: {
+        marginTop: 10,
+        backgroundColor: '#FFF',
+    },
+    button: {
+        backgroundColor: '#080',
+        marginTop: 10,
+        padding: 10,
+        alignItems: 'center',
+        borderRadius: 25
+    },
+    buttonText: {
+        fontFamily: commonStyles.fontFamily,
+        color: '#FFF',
+        fontSize: 20
+    },
+    buttonTextOptions: {
+        fontFamily: commonStyles.fontFamily,
+        color: "black",
+        fontSize: 18,
+        fontWeight:'bold'
+    }
+})
